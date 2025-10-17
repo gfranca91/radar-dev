342c824b2816c11307db329aa806b91c150d9cab
gfranca91 <gfrancavieira@gmail.com>
2025-10-17 11:55:40 -0300

    Improve layout, add logo and favicon, implement numeric pagination

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,20 @@
 import Link from "next/link";
 import { FaGithub, FaInstagram, FaTwitter } from "react-icons/fa";
+import Image from "next/image";
 
 export default function Header() {
   return (
     <header className="bg-gray-200 border-b border-gray-200 sticky top-0 z-50">
       <div className="container mx-auto p-4 flex justify-between items-center">
         <Link href="/" className="text-2xl font-bold text-gray-900">
-          Radar Dev 🚀
+          <Image
+            src="/RadarDev.png"
+            alt="Radar Dev logo"
+            width={220}
+            height={50}
+            priority
+            className="max-w-full h-auto"
+          />
         </Link>
 
         <div className="flex items-center space-x-4">
